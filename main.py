791e01c711f2c208d287679dae28ac62ee59152c
mauza <mauza@mauza.net>
2021-05-27 20:57:17 -0600

    added blinking ir led

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import messages
 
 def main():
     sensor = gpio.IR_Sensor(14)
+    ir_led = gpio.IR_LED(15)
     sensor.stream_input()
+    ir_led.blink(on_time=0.1, off_time=0.1)
     time.sleep(10)
     sensor.stop_stream()
 
