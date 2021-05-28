39029c7f33b5a800c21adc20dbe6c682a979e7be
mauza <mauza@mauza.net>
2021-05-27 20:49:46 -0600

    some work to get IR sensor raw output

---
+++ gpio.py
@@ -0,0 +1,44 @@
+import time
+from threading import Thread
+
+import gpiozero
+
+STOP_THREAD = False
+
+class IR_Sensor:
+
+    def __init__(self, pin_num):
+        self.pin_num = pin_num
+        self.sensor = gpiozero.DigitalInputDevice(pin_num)
+
+    def value(self):
+        return self.sensor.value
+
+    def stream_input(self):
+        self._thread = Thread(target=self._stream_input, args=(lambda: STOP_THREAD))
+        self._thread.start()
+
+    def _stream_input(self, stop):
+        while True:
+            if stop():
+                break
+            print(self.value())
+            time.sleep(0.01)
+
+    def stop_stream(self):
+        if self._thread:
+            STOP_THREAD = True
+            self._thread.join()
+            STOP_THREAD = False
+
+
+
+
+
+class IR_LED:
+    def __init__(self, pin_num):
+        self.pin_num = pin_num
+        self.LED = gpiozero.LED(pin_num)
+
+    def blink(self, *args, **kwargs):
+        return self.LED.blink(*args, **kwargs)
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+import time
+import gpio
+import messages
 
 
 def main():
-    pass
+    sensor = gpio.IR_Sensor(14)
+    sensor.stream_input()
+    time.sleep(10)
+    sensor.stop_stream()
 
 
 if __name__ == "__main__":
