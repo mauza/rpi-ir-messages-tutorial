df701884e78f56da92e1819e2da2545b4c0cfa47
mauza <mauza@mauza.net>
2021-05-27 21:03:46 -0600

    changed ir led blink length

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     sensor = gpio.IR_Sensor(14)
     ir_led = gpio.IR_LED(15)
     sensor.stream_input()
-    ir_led.blink(on_time=0.1, off_time=0.1)
+    ir_led.blink(on_time=0.5, off_time=0.5)
     time.sleep(10)
     sensor.stop_stream()
 
