c342bb9674f75ef45cc765a558914c17658dadbe
mauza <mauza@mauza.net>
2021-05-31 10:35:46 -0600

    slight buffer refactor

--- src/library.py
+++ src/library.py
@@ -3,6 +3,8 @@ from threading import Thread
 
 import gpiozero
 
+POLL_INTERVAL = 0.001
+
 
 def serialize_message(message):
     return [ord(character) for character in message]
@@ -13,7 +15,6 @@ def deserialize_message(ascii_list):
 
 
 class Buffer:
-
     def __init__(self, size):
         self.size = size
         self.buffer = []
@@ -34,7 +35,7 @@ class Buffer:
 
 
 class IR_Sensor:
-    threshold = 0.4
+    threshold = 0.1
 
     def __init__(self, pin_num):
         self.pin_num = pin_num
@@ -53,37 +54,35 @@ class IR_Sensor:
     def _convert_input(self, stop):
         start_time = time.time()
         ascii_list = []
-        long_buffer = Buffer(20)
-        short_buffer = Buffer(3)
+        raw_buffer = Buffer(20)
+        value_buffer = Buffer(10)
         previous_value = 0
         tmp_ascii = 0
         while True:
             if stop():
                 break
             raw_value = self.raw_q.get()
-            short_buffer.put(raw_value)
-            long_buffer.put(raw_value)
-            short_value = short_buffer.value()
-            long_value = long_buffer.value()
-            print(f"values: {short_value} --- {long_value}")
-            if short_value < 0:
+            raw_buffer.put(raw_value)
+            value = raw_buffer.value()
+            value_buffer.put(value)
+            print(f"values: {value}")
+            if value < 0:
                 continue
-            elif previous_value == 0 and long_value <= 0.1:
+            elif previous_value == 0 and value_buffer.value == 0:
                 ascii_list.append(tmp_ascii)
                 tmp_ascii = 0
                 continue
-            elif short_value <= self.threshold:
-                previous_value = 0
-            elif short_value > self.threshold:
-                if previous_value == 0:
-                    print(f"ascii tmp value: {tmp_ascii}")
+            elif value <= self.threshold:
+                if previous_value == 1:
                     tmp_ascii += 1
+                previous_value = 0
+            elif value > self.threshold:
                 previous_value = 1
 
             current_time = time.time()
-            if current_time - start_time >= 15:
+            if len(tmp_ascii) > 2 or current_time - start_time >= 20:
                 start_time = current_time
-                print(deserialize_message(ascii_list))
+                print(deserialize_message(ascii_list), end='')
                 ascii_list = []
 
 
@@ -97,7 +96,7 @@ class IR_Sensor:
             else:
                 value = 0
             self.raw_q.put(value)
-            time.sleep(0.001)
+            time.sleep(POLL_INTERVAL)
 
     def stop(self):
         self._stop = True
@@ -115,8 +114,8 @@ class IR_LED:
         self.LED = gpiozero.LED(pin_num)
 
     def blink(self, n):
-        on_time = 0.01
-        off_time = 0.01
+        on_time = POLL_INTERVAL * 10
+        off_time = POLL_INTERVAL * 10
         self.LED.blink(on_time, off_time, n=n, background=False)
 
     def off(self):
@@ -130,5 +129,5 @@ class IR_LED:
         for code in encoded_msg:
             print(f"sending ascii code: {code}")
             self.blink(n=code)
-            time.sleep(0.15)
+            time.sleep(POLL_INTERVAL*100)
 
