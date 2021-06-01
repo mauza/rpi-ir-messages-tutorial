0902e82f03a4e71dafcc47804096702dfadae574
mauza <mauza@mauza.net>
2021-06-01 07:08:00 -0600

    increase off_iter

--- src/library.py
+++ src/library.py
@@ -77,7 +77,7 @@ class IR_Sensor:
                 off_iter = 0
                 previous_value = 1
 
-            if off_iter >= POLL_INTERVAL*50000:
+            if off_iter >= POLL_INTERVAL*500000:
                 ascii_code = sum(value_buffer.buffer)
                 if ascii_code == 0:
                     continue
