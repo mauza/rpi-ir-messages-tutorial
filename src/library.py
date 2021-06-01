24bc2dc22f4db800833d5e941829ad7bf7bf665f
mauza <mauza@mauza.net>
2021-05-31 21:31:57 -0600

    15->16

--- src/library.py
+++ src/library.py
@@ -117,7 +117,7 @@ class IR_LED:
     def __init__(self, pin_num):
         self.pin_num = pin_num
         self.LED = gpiozero.LED(pin_num)
-        self.blink_interval = POLL_INTERVAL * 15
+        self.blink_interval = POLL_INTERVAL * 16
 
     def blink(self, n):
         on_time = self.blink_interval
