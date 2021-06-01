400692359ddda134d4987a27fbbb27903b7fc18e
mauza <mauza@mauza.net>
2021-06-01 07:53:25 -0600

    off_iter

--- src/library.py
+++ src/library.py
@@ -79,7 +79,7 @@ class IR_Sensor:
                 off_iter = 0
                 previous_value = 1
 
-            if off_iter >= 500:
+            if off_iter >= 1000:
                 ascii_code = sum(value_buffer.buffer)
                 if ascii_code == 0:
                     continue
